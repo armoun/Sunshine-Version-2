ddf730e69594f969d9d4eae8140b0f540a8f6550
Mohsen Armoun <armoun.mohsen@gmail.com>
2016-11-26 22:12:16 +0330

    fix deprecated
    change the SettingsActivity
    becuse addpreferencesfromresource and findPreference deprecated , i used
    a PreferencesFragment for use
    addpreferencesfromresource and findPreference and then via
    getFragmentManager().beginTransaction() send the Preference to the
    onCreate Method

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -31,16 +31,33 @@ import android.preference.PreferenceManager;
  */
 public class SettingsActivity extends PreferenceActivity
         implements Preference.OnPreferenceChangeListener {
+			
+			public static class Prefs extends PreferenceFragment{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.pref_general);
+
+        // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
+        // updated when the preference changes.
+        SettingsActivity settings = new SettingsActivity();
+        settings.bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+
+    }
+
+}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Add 'general' preferences, defined in the XML file
-        addPreferencesFromResource(R.xml.pref_general);
-
+        //addPreferencesFromResource(R.xml.pref_general);
         // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
         // updated when the preference changes.
-        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+        //bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+		        getFragmentManager().beginTransaction().replace(android.R.id.content, new Prefs()).commit();
+
     }
 
     /**
